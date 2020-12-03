4cf60b2e30a201c9a30cd340cba4837921045dfa
请不要阻拦我学习 <924370430@qq.com>
2020-12-03 20:55:24 +0800

    初始化项目

---
+++ src/index.js
@@ -0,0 +1,24 @@
+/*
+ * @Author: DaZheng
+ * @Date: 2020-12-03 20:10:00
+ * @LastEditors: g05047
+ * @LastEditTime: 2020-12-03 20:12:17
+ * @Description: file content
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  // <React.StrictMode>
+    <App />
+  // </React.StrictMode>
+  ,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
