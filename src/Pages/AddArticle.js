3894a6f93d46b7a939a1600d1bd0ceec9d5fde7e
请不要阻拦我学习 <924370430@qq.com>
2020-12-04 22:41:45 +0800

    添加修改文章

--- src/Pages/AddArticle.js
+++ src/Pages/AddArticle.js
@@ -2,13 +2,13 @@
  * @Author: DaZheng
  * @Date: 2020-12-03 22:57:43
  * @LastEditors: g05047
- * @LastEditTime: 2020-12-04 18:48:45
+ * @LastEditTime: 2020-12-04 22:40:46
  * @Description: file content
  */
 import React, { useState, useEffect, useReducer } from 'react'
 import marked from 'marked'
 import '../static/css/AddArticle.css'
-import { Row, Col, Input, Select, Button, DatePicker } from 'antd'
+import { Row, Col, Input, Select, Button, DatePicker, message } from 'antd'
 import axios from 'axios'
 
 import servicePath from '../config/apiUrl'
@@ -33,7 +33,7 @@ function AddArticle (props) {
   // eslint-disable-next-line
   const [typeInfo ,setTypeInfo] = useState([]) // 文章类别信息
   // eslint-disable-next-line
-  const [selectedType,setSelectType] = useState('请选择类型') //选择的文章类别
+  const [selectedType,setSelectType] = useState('选择类型') //选择的文章类别
 
   useEffect( () => {
     getTypeInfo()
@@ -79,6 +79,74 @@ function AddArticle (props) {
     ) 
   }
 
+  const selectTypeHandler = (value) => {
+    setSelectType(value)
+  }
+
+  const saveArticle = () => {
+    if (!selectedType) {
+      message.error('必须选择文章类型')
+      return false
+    } else if (!articleTitle) {
+      message.error('文章名称不能为空')
+      return false
+    } else if (!articleContent) {
+      message.error('文章内容不能为空')
+      return false
+    } else if (!introducemd) {
+      message.error('文章简介不能为空')
+      return false
+    } else if (!showDate) {
+      message.error('发布日期不能为空')
+      return false
+    } else if (selectedType === '选择类型') {
+      message.error('请选择类型')
+      return false
+    }
+    let dataProps = {}
+    dataProps.type_id = selectedType
+    dataProps.title = articleTitle
+    dataProps.article_content = articleContent
+    dataProps.introduce = introducemd
+    let dateText = showDate.replace('-', '/')
+    dataProps.addTime = (new Date(dateText).getTime() / 1000)
+
+    if (articleId == 0) {
+      dataProps.view_count = 0
+      axios({
+        method: 'post',
+        url: servicePath.addArticle,
+        data: dataProps,
+        withCredentials: true
+      }).then(
+        res => {
+          setArticleId(res.data.insertId)
+          if (res.data.isSuccess) {
+            message.success('文章添加成功')
+          } else {
+            message.success('文章添加失败')
+          }
+        }
+      )
+    } else {
+      dataProps.id = articleId
+      axios({
+        method: 'post',
+        url: servicePath.updateArticle,
+        data: dataProps,
+        withCredentials: true
+      }).then(
+        res => {
+          if (res.data.isSuccess) {
+            message.success('文章修改成功')
+          } else {
+            message.error('修改失败')
+          }
+        }
+      )
+    }
+  }
+
   return (
     <div>
       <Row gutter={5}>
@@ -86,13 +154,15 @@ function AddArticle (props) {
           <Row gutter={10}>
             <Col span="20">
               <Input 
+                value={articleTitle}
                 placeholder="博客标题"
                 size="large"
+                onChange={e => setArticleTitle(e.target.value)}
               />
             </Col>
             <Col span={4}>
               &nbsp;
-              <Select defaultValue={selectedType} size="large">
+              <Select defaultValue={selectedType} size="large" onChange={selectTypeHandler}>
                 {
                   typeInfo.map((item, index) => {
                     return (<Option key={index} value={item.id}>{item.typeName}</Option>)
@@ -123,7 +193,7 @@ function AddArticle (props) {
           <Row>
             <Col span="24">
               <Button size="large">暂存文章</Button>&nbsp;
-              <Button type="primary" size="large">发布文章</Button>
+              <Button type="primary" size="large" onClick={saveArticle}>发布文章</Button>
               <br/>
             </Col>
             <Col span="24">
@@ -143,6 +213,7 @@ function AddArticle (props) {
             <Col span={12}>
               <div className="date-select">
                 <DatePicker 
+                  onChange={(date, dateString) => setShowDate(dateString)}
                   placeholder="发布日期"
                   size="large"
                 />
--- src/config/apiUrl.js
+++ src/config/apiUrl.js
@@ -2,7 +2,7 @@
  * @Author: DaZheng
  * @Date: 2020-12-04 14:41:55
  * @LastEditors: g05047
- * @LastEditTime: 2020-12-04 19:05:02
+ * @LastEditTime: 2020-12-04 22:33:31
  * @Description: file content
  */
 //  let ipUrl = 'http://127.0.0.1:7001/admin/'
@@ -11,6 +11,8 @@ let ipUrl = 'http://localhost:7001/admin/'
 let servicePath = {
   checkLogin: ipUrl + 'checkLogin', //检查用户名密码
   getTypeInfo: ipUrl + 'getTypeInfo', //获得文章类别信息
+  addArticle: ipUrl + 'addArticle', //添加文章
+  updateArticle: ipUrl + 'updateArticle' //修改文章
 }
 
 export default servicePath
